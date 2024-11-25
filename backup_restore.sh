c21614c4ad3568fbf8df5c7338dac8cd0ca1fcaf
spmanolo <manolosan2001@gmail.com>
2024-11-25 12:51:58 +0100

    Enhance backup and restore functionality

--- backup_restore.sh
+++ backup_restore.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# FORMATO: mongorestore --db tienda --drop ./backups/<fecha_backup>/tienda/
-
 # Variables
 BACKUP_DATE="24-11-24"
-BACKUP_DIR="./backups/$BACKUP_DATE"
+BACKUP_DIR="/backups/$BACKUP_DATE"
 DB_NAME="tienda"
-IMAGES_DIR="./e-commerce/static/img"
+IMAGES_DIR="/e-commerce/static/img"
 
-# mongorestore --db tienda --drop ./backups/01-09-24/tienda/
-mongorestore --db $DB_NAME --drop $BACKUP_DIR/$DB_NAME/
+# mongorestore --db tienda --drop ./backups/24-11-24/tienda/
+mongorestore --uri="mongodb://mongo:27017" --db $DB_NAME --drop $BACKUP_DIR/$DB_NAME/
 
 # Restaurar las imágenes
 if [ -d "$BACKUP_DIR/img" ]; then
+  rm -rf $IMAGES_DIR
   mkdir -p $IMAGES_DIR
   cp -r $BACKUP_DIR/img/* $IMAGES_DIR/
   echo "Imágenes restauradas correctamente."
